c1a145936d55eab36260aa8ebc044fa743322c26
damien chancerel <damien.chancerel@gmail.com>
2019-12-10 12:23:54 +0100

    footer #2

--- index.php
+++ index.php
@@ -529,7 +529,7 @@
           <!-- partial:partials/_footer.html -->
           <footer class="footer">
             <div class="d-sm-flex justify-content-center justify-content-sm-between">
-              <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Projet Devleoppement Web 2019 - Polytech Paris-Sud  <a href="http://www.polytech.u-psud.fr/fr/index.html" target="_blank">Polytech</a></span>
+              <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Projet Developpement Web 2019 -  <a href="http://www.polytech.u-psud.fr/fr/index.html" target="_blank">Polytech Paris-Sud</a></span>
               <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i></span>
             </div>
           </footer>
