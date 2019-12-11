25fbbcfe415947f1ea4f9e084f86292fb5b52d56
STEVEN KERAUTRET <steven928@hotmail.fr>
2019-12-11 12:18:41 +0100

    Add a new event

--- register.php
+++ register.php
@@ -20,7 +20,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
  
     // Validate username
     if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
+        $username_err = "Veuillez rentrer un nom d'utilisateur.";
     } else{
         // Prepare a select statement
         $sql = "SELECT id FROM users WHERE username = ?";
