661700fef9e551109c3789d29d34ac53f46fb613
StevenK8 <43873889+StevenK8@users.noreply.github.com>
2021-11-17 16:57:29 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'ryzen.ddns.net:3306');
-   define('DB_USERNAME', 'payfriends-remote');
-   define('DB_PASSWORD', 'password');
+   define('DB_SERVER', 'xx:3306');
+   define('DB_USERNAME', 'user');
+   define('DB_PASSWORD', 'password;');
    define('DB_DATABASE', 'payfriends');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
